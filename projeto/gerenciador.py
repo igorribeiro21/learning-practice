a0d598fbbeee7f3ce73e212bb2644c8452d41e06
igor.ribeiro <igorapalvesribeiro@gmail.com>
2024-04-26 15:09:42 -0300

    main - feat: finished the task manager project

--- projeto/gerenciador.py
+++ projeto/gerenciador.py
@@ -10,6 +10,29 @@ def ver_tarefas(tarefas):
         status = "✓" if tarefa["completada"] else " "
         nome_tarefa = tarefa["tarefa"]
         print(f"{indice}. [{status}] {nome_tarefa}")
+    return
+
+def atualizar_nome_tarefa(tarefas,indice_tarefa,novo_nome_tarefa):
+    indice_tarefa_ajustado = int(indice_tarefa) - 1
+    if indice_tarefa_ajustado >= 0 and indice_tarefa_ajustado < len(tarefas):
+        tarefas[indice_tarefa_ajustado]["tarefa"] = novo_nome_tarefa
+        print(f"Tarefa {indice_tarefa} atualizada para {novo_nome_tarefa}")
+    else:
+        print("Indice de tarefa inválido.")
+    return
+
+def completar_tarefa(tarefas, indice_tarefa):
+    indice_tarefa_ajustado = int(indice_tarefa) - 1
+    tarefas[indice_tarefa_ajustado]["completada"] = True
+    print(f"Tarefa {indice_tarefa} marcada como completada")
+    return
+
+def deletar_tarefas_completadas(tarefas):
+    for tarefa in tarefas:
+        if tarefa["completada"]:
+            tarefas.remove(tarefa)
+    print("Tarefas completadas foram deletadas")
+    return
 
 
 tarefas = []
@@ -28,6 +51,18 @@ while True:
         nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
         adicionar_tarefa(tarefas, nome_tarefa)
     elif escolha == "2":
+        ver_tarefas(tarefas)
+    elif escolha == "3":
+        ver_tarefas(tarefas)
+        indice_tarefa = input("Digite o número da tarefa que deseja atualizar: ")
+        novo_nome = input("Digite o novo nome da tarefa: ")
+        atualizar_nome_tarefa(tarefas,indice_tarefa,novo_nome)
+    elif escolha == "4":
+        ver_tarefas(tarefas)
+        indice_tarefa = input("Digite o número da tarefa que deseja completar: ")
+        completar_tarefa(tarefas,indice_tarefa)
+    elif escolha == "5":
+        deletar_tarefas_completadas(tarefas)
         ver_tarefas(tarefas)
     elif escolha == "6":
         break
