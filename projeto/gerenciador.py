3c8c6ea3a828246e144a8faf20461ce1d192666b
igor.ribeiro <igorapalvesribeiro@gmail.com>
2024-04-26 11:39:32 -0300

    main - feat: created in the project the functionalities of adding and viewing tasks

--- projeto/gerenciador.py
+++ projeto/gerenciador.py
@@ -1,3 +1,18 @@
+def adicionar_tarefa(tarefas, nome_tarefa):
+    tarefa = {"tarefa": nome_tarefa, "completada": False}
+    tarefas.append(tarefa)
+    print(f"Tarefa {nome_tarefa} foi adicionada com sucesso!")
+    return
+
+def ver_tarefas(tarefas):
+    print("\nLista de tarefas:")
+    for indice, tarefa in enumerate(tarefas,start=1):
+        status = "✓" if tarefa["completada"] else " "
+        nome_tarefa = tarefa["tarefa"]
+        print(f"{indice}. [{status}] {nome_tarefa}")
+
+
+tarefas = []
 while True:
     print("\n Menu do Gerenciador de Lista de tarefas:")
     print("1. Adicionar tarefa")
@@ -9,7 +24,12 @@ while True:
 
     escolha = input("Digite a sua escolha: ")
 
-    if escolha == "6":
+    if escolha == "1":
+        nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
+        adicionar_tarefa(tarefas, nome_tarefa)
+    elif escolha == "2":
+        ver_tarefas(tarefas)
+    elif escolha == "6":
         break
 
 print("Programa finalizado")
